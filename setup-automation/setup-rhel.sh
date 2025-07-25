b0ef04977dc797044e2ddc9442b0acad9da595a0
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2025-07-25 09:54:45 -0500

    Update setup-rhel.sh

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -13,20 +13,20 @@ echo "Origins = https://cockpit-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
 echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
 systemctl enable --now cockpit.socket
 
-echo "Adding wheel" > /root/post-run.log
-usermod -aG wheel rhel
+#echo "Adding wheel" > /root/post-run.log
+#usermod -aG wheel rhel
 
-echo "setting password" >> /root/post-run.log
-echo redhat | passwd --stdin rhel
+#echo "setting password" >> /root/post-run.log
+#echo redhat | passwd --stdin rhel
 
-echo "exclude=kernel*" >> /etc/yum.conf
+#echo "exclude=kernel*" >> /etc/yum.conf
 
-echo "Install PCP packages" >> /root/post-run.log
-dnf install pcp-zeroconf cockpit-pcp stress-ng -y
+#echo "Install PCP packages" >> /root/post-run.log
+#dnf install pcp-zeroconf cockpit-pcp stress-ng -y
 
-echo "restart cockpit" >> /root/post-run.log
-systemctl restart cockpit
+#echo "restart cockpit" >> /root/post-run.log
+#systemctl restart cockpit
 
-echo "DONE" >> /root/post-run.log
+#echo "DONE" >> /root/post-run.log
 
-touch /root/post-run.log.done
+#touch /root/post-run.log.done
