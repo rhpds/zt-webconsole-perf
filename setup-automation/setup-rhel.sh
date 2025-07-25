c8cbd6f5023c2275a7ed7904d2f6bf81fd182751
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2025-07-25 09:14:29 -0500

    Remove instruq wait while loop

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
-do
-   echo "Waiting for Instruqt to finish booting the VM"
-   sleep 1
-done
 
 subscription-manager config --rhsm.manage_repos=1
 subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force
